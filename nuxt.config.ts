7e74c7566e5fa0df89a29dda33509b76c968a589
Simone <151656886+simone013124@users.noreply.github.com>
2025-10-18 13:36:06 +0200

    changes 4 deployment on render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,15 @@
-import { defineNuxtConfig } from 'nuxt'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  components: true,
+  components: true, // Auto-Import aktivieren
   modules: ['@nuxt/image'],
-
-  vite: {
-    plugins: [tailwindcss()]
+    vite: {
+    plugins: [
+      tailwindcss()],
   },
-
-  css: ['../app/assets/main.css'],
-
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: '0.0.0.0'
-  }
+   css: ['../app/assets/main.css']
 })
