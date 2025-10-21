00df4047822ef7ec2fc4ba5913c3f3f5c76bd938
s2210456017@fhooe.at <s2210456017@fhooe.at>
2025-10-21 18:51:28 +0200

    deployment and seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+    app: {
+    head: {
+      title: 'Ferienhaus Pürstl – Urlaub an der Reiteralm',
+      meta: [
+        { name: 'description', content: 'Im Ferienhaus Pürstl beginnt dein Bergurlaub direkt an der Reiteralm – egal ob Skifahren im Winter oder Wandern im Sommer.' },
+        { property: 'og:title', content: 'Ferienhaus Pürstl – Urlaub an der Reiteralm' },
+        { property: 'og:description', content: 'Bergurlaub direkt an der Reiteralm – Ruhe, Natur und Komfort im Ferienhaus Pürstl.' },
+        { property: 'og:type', content: 'website' },
+      ],
+      htmlAttrs: { lang: 'de' },
+    }
+  },
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
+
   devtools: { enabled: true },
 
   components: true, // Auto-Import aktivieren
@@ -12,11 +29,4 @@ export default defineNuxtConfig({
       tailwindcss()],
   },
   css: ['../app/assets/main.css'],
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'de'
-      }
-    }
-  }
 })
